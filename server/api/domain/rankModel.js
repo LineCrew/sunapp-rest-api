89d31fc59476a1d89e8b36a99ba830d5adfbf2c4
ParkJude <park.jude.96@gmail.com>
2018-02-09 14:33:43 +0000

    RankModel 수정

--- server/api/domain/rankModel.js
+++ server/api/domain/rankModel.js
@@ -34,9 +34,10 @@ export default class RankModel {
       return result;
     } else if (this.rankType === 'single') {
       // TODO: UserAnswerEntity -> User 로 관계를 바꿔야함.
-      const targetUserAnswerEntity = await sequelize.query(`select count(*) as answerCount, user_id from answers as a left outer join users as u on u.id = a.user_id where a.created_at BETWEEN DATE_SUB(NOW(), INTERVAL 30 DAY) AND NOW() and a.isCorrect = true and a.gameType = ${this.rankType} group by user_id order by answerCount desc;`);
-      return targetUserAnswerEntity;
+      const targetRankEntities = await sequelize.query(`select count(*) as answerCount, user_id from answers as a left outer join users as u on u.id = a.user_id where a.created_at BETWEEN DATE_SUB(NOW(), INTERVAL 30 DAY) AND NOW() and a.isCorrect = true and a.gameType = '${this.rankType}' group by user_id order by answerCount desc;`);
       
+      return targetRankEntities[0];
+
     } else if (this.rankType === 'friend') {
       const result = UserEntity.findAll({
       });
