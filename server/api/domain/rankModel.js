901f8dbd89880c20058b4272fe063217d81409e1
Dainel Park <topbladep@gmail.com>
2018-02-11 16:19:48 +0900

    gameResult Type 의 Ranking Query 를 추가

--- server/api/domain/rankModel.js
+++ server/api/domain/rankModel.js
@@ -14,28 +14,22 @@ export default class RankModel {
    */
   async getRankByType(state = {}) {
     if (this.rankType === 'gameResult') {
-      const result = await GameResultEntity.findAll({
-        attributes: { include: [[sequelize.fn('COUNT', sequelize.col('winnerUserId.id')), 'winCount']] },
-        include: [{
-          model: UserEntity,
-          as: 'users',
-        }],
-        where: {
-          createdAt: {
-            [Op.lt]: new Date(),
-            [Op.gt]: new Date(new Date() - week),
-          },
-        },
-        group: 'winnerUserId.id',
-        limit: state.limit,
-        order: [['winCount', 'DESC']],
-      });
+      const targetRankEntities = await sequelize.query(
+        `select 
+          *, 
+          count(*) as winCount 
+        from gameResult as g 
+        left outer users as u.id = g.winnerUserId 
+        where a.created_at BETWEEN DATE_SUB(NOW(), INTERVAL 7 DAY) AND NOW() 
+        group by g.winnerUserId 
+        order by winCount asc`,
+      );
 
-      return result;
+      return targetRankEntities[0];
     } else if (this.rankType === 'single') {
       // TODO: UserAnswerEntity -> User 로 관계를 바꿔야함.
-      const targetRankEntities = await sequelize.query(`select *, count(*) as answerCount, user_id from answers as a left outer join users as u on u.id = a.user_id where a.created_at BETWEEN DATE_SUB(NOW(), INTERVAL 7 DAY) AND NOW() and a.isCorrect = true and a.gameType = '${this.rankType}' group by user_id order by answerCount desc;`);
-      
+      const targetRankEntities = await sequelize.query(`select *, count(*) as answerCount, user_id from answers as a left outer join users as u on u.id = a.user_id where a.created_at BETWEEN DATE_SUB(NOW(), INTERVAL 7 DAY) AND NOW() and a.isCorrect = true and a.gameType = '${this.rankType}' group by user_id order by answerCount asc;`);
+
       return targetRankEntities[0];
 
     } else if (this.rankType === 'friend') {
